88170ab893f515595139481375973250095929a8
Godson <godsonemmanuel07@gmail.com>
2024-01-21 14:31:33 +0530

    updated Login page layout

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -7,9 +7,17 @@ const AuthLayout = () => {
       {isAuthenticated ? (
         <Navigate to="/" />
       ) : (
-        <section>
-          <Outlet />
-        </section>
+        <>
+          <section className="flex flex-1 justify-center items-center flex-col pt-10">
+            <Outlet />
+          </section>
+
+          <img
+            src="/assets/images/side-img.svg"
+            alt="logo"
+            className="hidden xl:block  h-screen w-1/2 object-cover bg-no-repeat"
+          />
+        </>
       )}
     </>
   );
