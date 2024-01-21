97190cdaf762f9dc973e39903ae8b83c242cec23
Godson <godsonemmanuel07@gmail.com>
2024-01-21 14:22:56 +0530

    initialized project files, and set up basic layout

---
+++ src/_auth/forms/SignupForm.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const SignupForm = () => {
+  return (
+    <div>
+      SignupForm
+      <Button>Click me</Button>
+    </div>
+  );
+};
+
+export default SignupForm;
