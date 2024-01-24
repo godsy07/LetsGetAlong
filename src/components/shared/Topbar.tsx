673cb48cfd9204ed3749f3795a18d5634dc3c8d1
Godson <godsonemmanuel07@gmail.com>
2024-01-24 10:34:04 +0530

    added topbar for logged in mobile devices

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -1,7 +1,50 @@
-import React from "react";
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import { Button } from "../ui/button";
+import { useSignInAccount } from "@/lib/react-query/queriesAndMutations";
+import { useUserContext } from "@/context/AuthContext";
 
 const Topbar = () => {
-  return <div>Topbar</div>;
+  const navigate = useNavigate();
+  const { user } = useUserContext();
+  const { mutate: signOut, isSuccess } = useSignInAccount();
+
+  useEffect(() => {
+    if (isSuccess) navigate(0);
+  }, [isSuccess]);
+
+  return (
+    <section className="topbar">
+      <div className="flex-between py-4 px-5">
+        <Link to="/" className="flex gap-3 items-center">
+          <img
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={130}
+            height={325}
+          />
+        </Link>
+
+        <div className="flex gap-4">
+          <Button
+            variant="ghost"
+            className="shad-button_ghost"
+            onClick={() => signOut()}
+          >
+            <img src="/assets/icons/logout.svg" alt="logout" />
+          </Button>
+        </div>
+        <Link to={`/profile/${user.id}`} className="flex-center gap-3">
+          <img
+            src={user.imageUrl || "/assets/images/profile-placeholder.svg"}
+            alt="profile"
+            className="h-8 w-8 rounded-full"
+          />
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default Topbar;
